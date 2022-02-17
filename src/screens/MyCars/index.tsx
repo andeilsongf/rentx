a7f2c8f5fe5aa257e69ab6013d582bbb27b7b909
Andeilson Ferreira <agf@Mac-Pro-de-Andeilson.local>
2022-02-17 11:54:49 -0300

    added float button

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,29 +1,37 @@
 import { useNavigation } from "@react-navigation/core";
-import { id } from "date-fns/locale";
 import React, { useState, useEffect } from "react";
 import { StatusBar, FlatList } from "react-native";
 import { useTheme } from "styled-components";
 import { BackButton } from "../../components/BackButton";
+import { AntDesign } from "@expo/vector-icons";
 import { CarDTO } from "../../dtos/CarDTO";
 import { Car } from "../../components/Car";
+import { Load } from "../../components/Load";
 import { api } from "../../services/api";
 
-import {
-    Container,
-    Header,
-    Title,
-    SubTitle,
-    Content,
-    Appointments,
-    AppointmentsTitle,
-    AppointmentsQuantity,
 
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+  CarWrapper,
+  CarFooter,
+  CarFooterTitle,
+  CarFooterPeriod,
+  CarFooterDate,
 } from "./styles";
 
 interface CarProps {
-    id: string;
-    user_id: string;
-    car: CarDTO;
+  id: string;
+  user_id: string;
+  car: CarDTO;
+  startDate: string;
+  endDate: string;
 }
 
 export function MyCars() {
@@ -60,41 +68,50 @@ export function MyCars() {
           backgroundColor="transparent"
         />
 
-        <BackButton
-            onPress={handleBack}
-            color={theme.colors.shape}
-        />
+        <BackButton onPress={handleBack} color={theme.colors.shape} />
         <Title>
           Escolha uma {"\n"}
           data de início e {"\n"}
           fim do aluguel
         </Title>
-        
-        <SubTitle>
-        Conforto, segurança e praticidade
-        </SubTitle>
 
-
+        <SubTitle>Conforto, segurança e praticidade</SubTitle>
       </Header>
 
-    <Content>
+      { loading ? <Load /> :
+      
+      <Content>
+     
         <Appointments>
-            <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
-            <AppointmentsQuantity>05</AppointmentsQuantity>
+          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+          <AppointmentsQuantity>{cars.length}</AppointmentsQuantity>
         </Appointments>
 
-    <FlatList 
-        data={cars}
-        keyExtractor={item => item.id}
-        showsVerticalScrollIndicator={false}
-        renderItem={({item}) => (
-            <Car data={item.car} />
-        )}
-
-    />
-
-    </Content>
-
+        <FlatList
+          data={cars}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <CarWrapper>
+              <Car data={item.car} />
+              <CarFooter>
+                <CarFooterTitle>Período</CarFooterTitle>
+                <CarFooterPeriod>
+                  <CarFooterDate>{item.startDate}</CarFooterDate>
+                  <AntDesign
+                    name="arrowright"
+                    size={20}
+                    color={theme.colors.title}
+                    style={{ marginHorizontal: 10 }}
+                  />
+                  <CarFooterDate>{item.endDate}</CarFooterDate>
+                </CarFooterPeriod>
+              </CarFooter>
+            </CarWrapper>
+          )}
+        />
+      </Content>
+      }
     </Container>
   );
 }
